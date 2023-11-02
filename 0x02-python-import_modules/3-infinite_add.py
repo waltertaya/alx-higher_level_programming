0959e2593d27cf99ed7841ea64a92d9eabeb89a4
waltertaya <waltermittybecker5@gmail.com>
2023-11-02 14:23:47 +0300

    import python

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the sum of all arguments."""
+    from sys import argv
+
+    sum = 0
+    for i in range(1, len(argv)):
+        sum += int(argv[i])
+    print(sum)
