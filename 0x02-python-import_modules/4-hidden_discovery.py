ba0d1808fe67aab927b480d559a96f6f5ab5b684
waltertaya <waltermittybecker5@gmail.com>
2023-11-02 15:44:03 +0300

    import python

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """Print the sum of all arguments."""
+    import hidden_4
+    
+    text = dir(hidden_4)
+    for i in range(len(text)):
+        if text[i][0] != '_':
+            print(text[i])
+    
