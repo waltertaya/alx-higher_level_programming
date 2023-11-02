d683c712c1e977bd658f1e0d1fc98e7e03f810b6
waltertaya <waltermittybecker5@gmail.com>
2023-11-02 12:28:22 +0300

    import in python

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,5 +9,5 @@ if __name__ == "__main__":
         print("1 argument:")
     else:
         print("{} arguments:".format(argc))
-    for i in range(1, argc + 1):
-        print("{}: {}".format(i, sys.argv[i]))
+    for i in range(argc):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
