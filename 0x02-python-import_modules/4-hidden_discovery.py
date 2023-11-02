d954baf740f16f65244c8dec5ab2a36789cc25ea
waltertaya <waltermittybecker5@gmail.com>
2023-11-02 16:38:51 +0300

    import python

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,11 +3,7 @@
 if __name__ == "__main__":
     """Print all names hidden in the compiled module hidden_4.pyc."""
     import hidden_4
-    
-    # text = dir(hidden_4)
-    # for i in range(0, len(text)):
-    #     if text[i][0] != '_':
-    #         print(text[i])
+
     names = dir(hidden_4)
     for name in names:
         if name[:2] != "__":
