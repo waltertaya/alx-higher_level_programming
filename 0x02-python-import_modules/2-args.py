8faa6fe8d660318a54422ee95c2092b30fb4d965
waltertaya <waltermittybecker5@gmail.com>
2023-11-02 12:26:17 +0300

    import in python

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,6 @@ if __name__ == "__main__":
     argc = len(sys.argv) - 1
     if argc == 0:
         print("0 argument.")
-        sys.exit()
     elif argc == 1:
         print("1 argument:")
     else:
