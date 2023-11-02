fc1a2bc7e7b3f8c5064e46d3f3c498a442f19822
waltertaya <waltermittybecker5@gmail.com>
2023-11-02 14:46:06 +0300

    import python

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,8 +5,6 @@ if __name__ == "__main__":
     import sys
 
     total = 0
-
     for i in range(1, len(sys.argv)):
         total += int(sys.argv[i])
-
-    print(total)
+    print("{:d}".format(total))
