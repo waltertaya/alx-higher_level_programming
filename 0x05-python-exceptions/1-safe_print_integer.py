1a71843f845fde775e0f404ca9ece04a8d6ddd56
waltertaya <waltermittybecker5@gmail.com>
2023-11-20 15:24:56 +0300

    value error and type error

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,5 +3,9 @@ def safe_print_integer(value):
     try:
         print('{:d}'.format(value))
         return True
+    # except ValueError:
+    #     return False
+    # except TypeError:
+    #     return False
     except (TypeError, ValueError):
         return False
