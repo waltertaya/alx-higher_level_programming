1ec3784f4e4c3509f9890c27f73382886c2b566b
waltertaya <waltermittybecker5@gmail.com>
2023-11-21 22:01:27 +0300

    area of a square using class

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
 
-
-'''class definition of a Square that defines a square by: (based on 2-square.py)'''
-
+"""Define a class Square."""
 
 
 class Square:
-    
-    
-    '''class definition of a Square that defines a square by: (based on 2-square.py)'''
+    """Represent a square."""
+
     def __init__(self, size=0):
-        '''Instantiation with optional size'''
-        self.__size = size
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError("size must be >=0")
-
-    def area(self):
-        '''Public instance method that returns the current square area'''
-        return self.__size ** 2
+            raise ValueError("size must be >= 0")
+        self.__size = size
+        
+        def area(self):
+            """Return the current area of the square."""
+            return (self.__size * self.__size)
