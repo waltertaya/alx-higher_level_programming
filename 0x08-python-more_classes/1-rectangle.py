fa409de53cc73cffdaef6eca28680f67a52b88a4
waltertaya <waltermittybecker5@gmail.com>
2023-11-27 18:44:33 +0300

    public instance methods

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -19,7 +19,7 @@ class Rectangle:
     def height(self, value):
         '''Setter for height'''
         if not isinstance(value, int):
-            raise TypeError("height must be integer")
+            raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
@@ -33,7 +33,7 @@ class Rectangle:
     def width(self, value):
         '''Setter for width'''
         if not isinstance(value, int):
-            raise TypeError("width must be integer")
+            raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -22,7 +22,7 @@ class Rectangle:
         '''Setter for width'''
         
         if not isinstance(value, int):
-            raise TypeError("width must be integer")
+            raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
@@ -37,7 +37,7 @@ class Rectangle:
     def height(self, value):
         '''Setter for width'''
         if not isinstance(value, int):
-            raise TypeError("height must be integer")
+            raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
