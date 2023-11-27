cb5110d39fd762108c289c32070b374a5d862ee3
waltertaya <waltermittybecker5@gmail.com>
2023-11-27 19:50:52 +0300

    public class attribute

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -55,7 +55,7 @@ class Rectangle:
             return ""
         rectangle_str = ""
         for _ in range(self.__height):
-            rectangle_str += Rectangle.print_symbol * self.__width + '\n'
+            rectangle_str += str(Rectangle.print_symbol) * self.__width + '\n'
         return rectangle_str.rstrip('\n')
 
     def __repr__(self):
