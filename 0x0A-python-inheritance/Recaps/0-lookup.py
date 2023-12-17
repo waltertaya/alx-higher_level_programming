17546b373b983e27b831922b68e22fc6b365bd59
waltertaya <waltermittybecker5@gmail.com>
2023-12-17 15:57:05 +0300

    updated

---
+++ 0x0A-python-inheritance/Recaps/0-lookup.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""function that returns a list of available attributes and methods of an object"""
+
+def lookup(obj):
+    """Returns a list of available attributes and methods of an object"""
+    return dir(obj)
