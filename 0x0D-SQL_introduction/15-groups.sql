3d6b78f402fd6666a11349c7628e2b8a9f855960
waltertaya <waltermittybecker5@gmail.com>
2024-01-16 21:40:46 +0300

    Updated advanced tasks

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,6 +1,6 @@
--- a script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in your MySQL server.
-
-SELECT score, COUNT(*) AS number_of_records
-FROM second_table
-GROUP BY score
-ORDER BY number_of_records DESC;
+-- Lists the number of records with the same score in the table second_table in my MySQL server.
+-- Records are ordered by descending count.
+SELECT `score`, COUNT(*) AS `number`
+FROM `second_table`
+GROUP BY `score`
+ORDER BY `number` DESC;
--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,12 +1,6 @@
--- a script that creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
-
-CREATE TABLE IF NOT EXISTS second_table (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(256) NOT NULL,
-    score INT NOT NULL,
-    PRIMARY KEY (id)
-);
-INSERT INTO second_table (name, score) VALUES ('John', 10);
-INSERT INTO second_table (name, score) VALUES ('Alex', 3);
-INSERT INTO second_table (name, score) VALUES ('Bob', 14);
-INSERT INTO second_table (name, score) VALUES ('George', 8);
+-- Creates and fills a table second_table with attributes id, name and score in my MySQL server with multiple rows.
+CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
