ee5e77ad08f0d3a22900aeaeabc5a8fdc28ae7a4
waltertaya <waltermittybecker5@gmail.com>
2024-01-17 23:24:17 +0300

    new file:   3-force_name.sql,4-never_empty.sql,5-unique_id.sql

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,7 @@
+-- script that creates the table force_name on your MySQL server.
+
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);
