9f74a53baca31061d143413b56c8573dbc22efe0
waltertaya <waltermittybecker5@gmail.com>
2024-01-17 23:28:00 +0300

    modified:   3-force_name.sql,4-never_empty.sql,5-unique_id.sql

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,7 +1,7 @@
 -- script that creates the table force_name on your MySQL server.
 
 CREATE TABLE IF NOT EXISTS force_name (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT,
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id)
 );
--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,7 +1,6 @@
 -- script that creates the table unique_id on your MySQL server.
 
 CREATE TABLE IF NOT EXISTS unique_id (
-    id INT NOT NULL AUTO_INCREMENT default 1 UNIQUE,
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY (id)
+    id INT default 1 UNIQUE,
+    name VARCHAR(256),
 );
