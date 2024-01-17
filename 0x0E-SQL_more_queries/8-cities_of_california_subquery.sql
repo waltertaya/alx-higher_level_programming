fd6bba1ea9680d20332c384499d36580330a6434
waltertaya <waltermittybecker5@gmail.com>
2024-01-17 23:50:34 +0300

    modified:   8-cities_of_california_subquery.sql

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,4 +2,4 @@
 -- The states table contains only one record where name = California (but the id can be different, as per the example) Results must be sorted in ascending order by cities.id
 -- You are not allowed to use the JOIN keyword The database name will be passed as an argument of the mysql command
 
-SELECT id, name FROM states s,cities c WHERE s.id = c.state_id AND s.name = 'California' ORDER BY c.id ASC;
+SELECT c.id, c.name FROM states s,cities c WHERE s.id = c.state_id AND s.name = 'California' ORDER BY c.id ASC;
