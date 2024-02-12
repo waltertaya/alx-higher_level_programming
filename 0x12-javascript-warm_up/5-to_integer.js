ed43a15fbbcdcd9a4e99b8e5ae9244e0cfe018e3
waltertaya <waltermittybecker5@gmail.com>
2024-02-12 13:51:17 +0300

    Contenation

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 
 const num = process.argv[2];
-if (process.argv.length === 2) {
-  console.log('Not a number');
-} else if (isNaN(num)) {
+// if (process.argv.length === 2) {
+//   console.log('Not a number');
+// } else 
+if (isNaN(num)) {
   console.log('Not a number');
 } else {
   console.log('My number: ', `${Math.trunc(num)}`);
