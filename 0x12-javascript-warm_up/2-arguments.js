288b363fc083d5454f19f1b4ecf86e892dc962b6
waltertaya <waltermittybecker5@gmail.com>
2024-02-12 13:20:50 +0300

    command line arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,13 +1,8 @@
 #!/usr/bin/node
 
-const { argv } = require('node:process');
-
+// const { argv } = require('node:process');
 // count arguments passed
-const len = argv.length;
-// console.log(len)
-// argv.forEach((val, index) => {
-//     console.log(`${index}: ${val}`);
-//   });
+const len = process.argv.length;
 if (len === 2) {
   console.log('No argument');
 } else if (len === 3) {
