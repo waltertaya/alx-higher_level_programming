3058a1b4ee850329943f5fa19af2f0ac4878ad6e
waltertaya <waltermittybecker5@gmail.com>
2024-02-12 13:57:35 +0300

    An integer parseInt

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,5 +4,8 @@ const num = process.argv[2];
 if (isNaN(num)) {
   console.log('Not a number');
 } else {
-  console.log('My number: ', `${Math.trunc(num)}`);
+  console.log('My number:', `${Math.trunc(num)}`);
 }
+// else {
+//     console.log('My number:', parseInt(num));
+// }
