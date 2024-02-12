2364cada9c7a8f171b22c572cd42491b94789b28
waltertaya <waltermittybecker5@gmail.com>
2024-02-12 13:53:13 +0300

    An Integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
 
 const num = process.argv[2];
-// if (process.argv.length === 2) {
-//   console.log('Not a number');
-// } else 
 if (isNaN(num)) {
   console.log('Not a number');
 } else {
