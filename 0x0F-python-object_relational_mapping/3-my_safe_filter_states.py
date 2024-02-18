cf58971766d9f49d951f3f863bde70f9adedf754
waltertaya <waltermittybecker5@gmail.com>
2024-02-18 22:38:51 +0300

    Updated: 4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -8,7 +8,7 @@ import sys
 
 if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+                           passwd=sys.argv[2], db=sys.argv[3])
     cur = conn.cursor()
     name = sys.argv[4]
     cur.execute("SELECT * FROM states WHERE name LIKE %s", (name,))
--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,14 +1,15 @@
-#!/usr/bin/env python3
-
+#!/usr/bin/python3
+""""""
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                           passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+                           passwd=sys.argv[2], db=sys.argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id")
+    cur.execute("""SELECT cities.id, cities.name, states.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id""")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
