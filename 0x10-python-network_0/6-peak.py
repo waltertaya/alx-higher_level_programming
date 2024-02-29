71ea166a9b630f48a21f31eec5266da782a6b07a
waltertaya <waltermittybecker5@gmail.com>
2024-03-01 01:06:30 +0300

    Updated: peak of the unsorted integers algorithm

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,27 @@
+#!/usr/bin/python3
+""" Find the peak in a list of unsorted integers """
+
+
 def find_peak(list_of_integers):
-    """ Function to find a peak in a list of integers """
-    if list_of_integers == []:
+    """ Find a peak in list_of_integers """
+
+    if list_of_integers is None or list_of_integers == []:
         return None
-    list_of_integers.sort()
-    return list_of_integers[-1]
+
+    list_len = len(list_of_integers)
+    if list_len == 1:
+        return list_of_integers[0]
+
+    if list_len == 2:
+        return max(list_of_integers)
+
+    mid = list_len // 2
+
+    if list_of_integers[mid] > list_of_integers[mid - 1] and \
+            list_of_integers[mid] > list_of_integers[mid + 1]:
+        return list_of_integers[mid]
+
+    if list_of_integers[mid] < list_of_integers[mid + 1]:
+        return find_peak(list_of_integers[mid + 1:])
+    else:
+        return find_peak(list_of_integers[:mid])
