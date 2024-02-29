88f43862f9eb15be23a959dbb32cb8dcff89ecdd
waltertaya <waltermittybecker5@gmail.com>
2024-03-01 01:09:34 +0300

    Only status code

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-# Script that sends a request to a URL passed as an argument, and displays only the status code of the response.
+# script that sends a request to a URL passed as an argument, and displays only the status code of the response. not allowed to use any pipe, redirection, ; and &&
+curl -s -o /dev/null -w "%{http_code}" "$1"
