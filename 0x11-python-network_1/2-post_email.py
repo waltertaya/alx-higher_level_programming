f627060b5459826d9ab7c08f55e3d1cc1896271e
waltertaya <waltermittybecker5@gmail.com>
2024-03-01 23:25:48 +0300

    Pycodestyle changed

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-""" script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and finally displays the body of the response. """
+""" script that takes in a URL and an email, sends a POST request to
+the passed URL with the email as a parameter, and finally displays
+the body of the response. """
 if __name__ == '__main__':
     import urllib.request
     import urllib.parse
     import sys
     url = sys.argv[1]
     email = sys.argv[2]
+    print("Your email is:", email)
     values = {'email': email}
     data = urllib.parse.urlencode(values)
     data = data.encode('ascii')
