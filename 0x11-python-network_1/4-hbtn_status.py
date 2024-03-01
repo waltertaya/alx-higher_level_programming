ba2fa2eb2b08eaf4b3815c5b811860dd1e4396dc
waltertaya <waltermittybecker5@gmail.com>
2024-03-01 23:42:26 +0300

    Added:  Response header value #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-""" script that fetches https://intranet.hbtn.io/status"""
+"""fetches https://intranet.hbtn.io/status."""
 import requests
 
-if __name__ == '__main__':
-    url = 'https://intranet.hbtn.io/status'
-    r = requests.get(url)
+
+if __name__ == "__main__":
+    r = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
