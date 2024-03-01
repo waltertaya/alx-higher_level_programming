20e9767aa73ae9d7c284de04175854a68c1c7cee
waltertaya <waltermittybecker5@gmail.com>
2024-03-01 23:38:29 +0300

    Added: 4-hbtn_status.py

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" script that fetches https://intranet.hbtn.io/status"""
+import requests
+
+if __name__ == '__main__':
+    url = 'https://intranet.hbtn.io/status'
+    r = requests.get(url)
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
