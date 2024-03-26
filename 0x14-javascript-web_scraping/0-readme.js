e180f25575611bc33a2cada81aaa06ff1f4ff2cb
waltertaya <waltermittybecker5@gmail.com>
2024-03-26 20:06:39 +0300

    Fixed: Task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
-<<<<<<< HEAD
-const fs = require('node:fs');
-=======
 const fs = require('fs');
->>>>>>> be915e6 (Fixed: Task 0)
 fs.readFile(process.argv[2], 'utf-8', function (error, content) {
   console.log(error || content);
 });
