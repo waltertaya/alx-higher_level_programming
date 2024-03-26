b0af7bf48e5091a209a0b1cfc1f5285553a113e4
waltertaya <waltermittybecker5@gmail.com>
2024-03-26 22:41:15 +0300

    Updated: Task 8 #advanced 101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,16 +1,36 @@
-#!/usr/bin/node
+// 101-starwars_characters.js
+
 const request = require('request');
-request('https://swapi-api.alx-tools.com/api/films/' + process.argv[2], (error, response, body) => {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    characters.forEach(character => {
-      request(character, (error, response, body) => {
-        if (!error) {
-          console.log(JSON.parse(body).name);
-        }
+
+const movieId = process.argv[2];
+
+const url = `https://swapi.dev/api/films/${movieId}/`;
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  } else {
+    const filmData = JSON.parse(body);
+    const charactersUrls = filmData.characters;
+    const characters = filmData.characters.map(characterUrl => {
+      return new Promise((resolve, reject) => {
+        request(characterUrl, (error, response, body) => {
+          if (error) {
+            reject(error);
+          } else {
+            const characterData = JSON.parse(body);
+            resolve(characterData.name);
+          }
+        });
       });
     });
-  } else {
-    console.log(error);
+
+    Promise.all(characters).then(names => {
+      names.forEach(name => {
+        console.log(name);
+      });
+    }).catch(error => {
+      console.error('Error:', error);
+    });
   }
 });
