7ecaaf2d63ff034e4334f36af0e71aade1774695
waltertaya <waltermittybecker5@gmail.com>
2024-03-26 19:31:11 +0300

    Updated: Task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
 const fs = require('node:fs');
 fs.readFile(process.argv[2], 'utf-8', function(err, data) {
-  if (err) {
-    console.log(err);
-    return;
-  }
-  console.log(data);
+    console.log(data || err)
 });
