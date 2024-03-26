389f39a4834a894378737d896772ae3fdee900f1
waltertaya <waltermittybecker5@gmail.com>
2024-03-26 21:49:20 +0300

    Updated: Task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-request(process.argv[2] + '/?', (error, response, body) => {
+request(process.argv[2], (error, response, body) => {
   if (error) {
     console.error('error:', error);
   }
