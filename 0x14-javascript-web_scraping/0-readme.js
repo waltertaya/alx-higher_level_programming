6601bd02942a8cfa61509c09c6ea628086329196
waltertaya <waltermittybecker5@gmail.com>
2024-03-26 19:29:12 +0300

    Updated: Taskk 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,8 @@
 #!/usr/bin/node
 const fs = require('node:fs');
-
-const filePath = process.argv[2];
-// Reading file synchronously
-// const data = fs.readFileSync(filePath, {encoding: utf-8});
-
-fs.readFile(filePath, 'utf-8', (err, data) => {
+fs.readFile(process.argv[2], 'utf-8', function(err, data) {
   if (err) {
-    console.error(err);
+    console.log(err);
     return;
   }
   console.log(data);
